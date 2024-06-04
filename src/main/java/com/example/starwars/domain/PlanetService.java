dff501779fa2db29a33e764ea55148f0f921169e
Monalysa <monalysaklauck@gmail.com>
2024-06-03 23:31:56 -0300

    feat: create - api

---
+++ src/main/java/com/example/starwars/domain/PlanetService.java
@@ -0,0 +1,12 @@
+package com.example.starwars.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlanetService {
+    private PlanetRepository PlanetRepository
+
+    public Planet create(Planet planet){
+
+    }
+}
---
+++ src/main/java/com/example/starwars/web/PlanetController.java
@@ -0,0 +1,23 @@
+package com.example.starwars.web;
+
+import com.example.starwars.domain.Planet;
+import com.example.starwars.domain.PlanetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/planets")
+public class PlanetController {
+
+    @Autowired
+    private PlanetService planetService;
+
+    @PostMapping
+    public ResponseEntity<Planet> create(@RequestBody Planet planet){
+
+    };
+}
